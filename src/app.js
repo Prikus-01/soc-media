53a366f3f0a277411e2dac9db51d21554ed8057f
Prikus-01 <9riyanshu.k@gmail.com>
2025-10-22 12:49:49 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Swagger/OpenAPI setup
-const serverUrl = process.env.BASE_URL || `http://localhost:${PORT}`;
+const serverUrl = process.env.BASE_URL || "/";
 const swaggerDefinition = {
   openapi: "3.0.3",
   info: {
